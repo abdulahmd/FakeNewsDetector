0065f7259910a668e85bb55b93e700da6f1ba617
Abdullah Ahmed <52929221+abdulahmd@users.noreply.github.com>
2024-06-30 10:46:23 -0500

    Add files via upload

--- fake_information/main.py
+++ fake_information/main.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, url_for, flash
+import os
 
-app = Flask (__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def main():
     return render_template('index.html')
+
